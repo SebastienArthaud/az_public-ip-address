2a2144d853943a9495b6ce3edb9d436187adb5dc
SebastienArthaud <63235835+SebastienArthaud@users.noreply.github.com>
2024-02-02 11:06:34 +0100

    Update main.tf
    changing the module source address

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "public_ip" {
-  source             = "../../az_public-ip-address"
+  source             = "github.com/SebastienArthaud/az_public-ip-address.git"
   resourcegroup_name = var.ip_resourcegroup_name
   ip_name            = var.ip_name
 }
